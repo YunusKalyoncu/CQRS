8844e00ed9fee69ca33f4b3648b406049f20a433
Yunus Kalyoncu <95497265+YunusKalyoncu@users.noreply.github.com>
2022-12-12 23:25:22 +0300

    Initial commit

---
+++ CQRS_İstanbulEğitimAkademi/CQRS_İstanbulEğitimAkademi/DAL/Context/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using CQRS_İstanbulEğitimAkademi.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS_İstanbulEğitimAkademi.DAL.Context
+{
+    public class ProductContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=YUNUS\\SQLEXPRESS;initial catalog=ZCQRSDb;integrated security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
